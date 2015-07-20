465b12c40e5629507684fef87b1e1acdc96acfb4
Tolgab <Mathman290@gmail.com>
2015-07-19 17:39:50 -0700

    Basic files

---
+++ Critic/Critic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Critic
+//
+//  Created by Tolga Beser on 7/19/15.
+//  Copyright (c) 2015 Tolga Beser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Critic/Critic/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Critic
+//
+//  Created by Tolga Beser on 7/19/15.
+//  Copyright (c) 2015 Tolga Beser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
